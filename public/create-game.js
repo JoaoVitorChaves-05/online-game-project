1600823cd2be869ec9816f954cf5e42644855b77
JoaoVitorChaves-05 <joaovmanciochaves@gmail.com>
2021-03-13 14:02:58 -0300

    first

---
+++ public/create-game.js
@@ -0,0 +1,159 @@
+// módulo de criar o jogo - adicionar, remover, movimentação e colisão
+export default function createGame() {
+    const state = {
+        players: {},
+        fruits: {},
+        screen: {
+            width: 10,
+            height: 10
+        }
+    }
+    const observers = []
+
+    function start() {
+        const frequency = 2000
+
+        setInterval(addFruit, frequency)
+    }
+
+    function subscribe(observerFunction) {
+        observers.push(observerFunction)
+    }
+
+    function notifyAll(command) {
+        for (const observerFunction of observers) {
+            observerFunction(command)
+        }
+    }
+
+    function setState(newState) {
+        Object.assign(state, newState);
+    }
+
+    function addPlayer(command) {
+        const playerId = command.playerId
+        const playerX = 'playerX' in command ? command.playerX : Math.floor(Math.random() * state.screen.width)
+        const playerY = 'playerY' in command ? command.playerY : Math.floor(Math.random() * state.screen.height)
+
+        state.players[playerId] = {
+            x: playerX,
+            y: playerY
+        }
+
+        notifyAll({
+            type: 'add-player',
+            playerId: playerId,
+            playerX: playerX,
+            playerY: playerY
+        })
+    }
+
+    function removePlayer(command) {
+        const playerId = command.playerId
+
+        delete state.players[playerId]
+
+        notifyAll({
+            type: 'remove-player',
+            playerId: playerId
+        })
+    }
+
+    function addFruit(command) {
+        const fruitId = command ? command.fruitId : Math.floor(Math.random() * 10000000)
+        const fruitX = command ? command.fruitX : Math.floor(Math.random() * state.screen.width)
+        const fruitY = command ? command.fruitY : Math.floor(Math.random() * state.screen.height)
+
+        state.fruits[fruitId] = {
+            x: fruitX,
+            y: fruitY
+        }
+
+        notifyAll({
+            type: "add-fruit",
+            fruitId: fruitId,
+            fruitX: fruitX,
+            fruitY: fruitY
+        })
+    }
+
+    function removeFruit(command) {
+        const fruitId = command.fruitId
+
+        delete state.fruits[fruitId]
+
+        notifyAll({
+            type: "remove-fruit",
+            fruitId: fruitId
+        })
+    }
+
+    function movePlayer(command) {
+        notifyAll(command)
+        console.log(`Moving ${command.playerId} with ${command.keyPressed}`)
+
+        const acceptedMoves = {
+            ArrowUp(player) {
+                if (keyPressed === 'ArrowUp' && player.y - 1 >= 0) {
+                    player.y = player.y - 1
+                    return
+                }
+            },
+
+            ArrowRight(player) {
+                if (keyPressed === 'ArrowRight' && player.x + 1 < state.screen.width) {
+                    player.x = player.x + 1
+                    return
+                }                        
+            },
+
+            ArrowDown(player) {
+                if (keyPressed === 'ArrowDown' && player.y + 1 < state.screen.height) {
+                    player.y = player.y + 1
+                    return
+                }
+            },
+
+            ArrowLeft(player) {
+                if (keyPressed === 'ArrowLeft' && player.x - 1 >= 0) {
+                    player.x = player.x - 1
+                    return
+                }
+            }
+        }
+
+        const keyPressed = command.keyPressed
+        const playerId = command.playerId
+        const player = state.players[playerId]
+        const moveFunction = acceptedMoves[keyPressed]
+
+        if (player && moveFunction) {
+            moveFunction(player)
+            checkForFruitCollision(playerId)
+        }
+        
+        function checkForFruitCollision(playerId) {
+            const player = state.players[playerId]
+
+            for (const fruitId in state.fruits) {
+                const fruit = state.fruits[fruitId]
+
+                if (player.x === fruit.x && player.y === fruit.y) {
+                    removeFruit({fruitId: fruitId})
+                }
+            }
+        }
+    }
+
+    return {
+        addPlayer,
+        removePlayer,
+        addFruit,
+        removeFruit,
+        movePlayer,
+        state,
+        setState,
+        subscribe,
+        start
+    }
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express'
+import http from 'http'
+import createGame from './public/create-game.js'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+const sockets = new Server(server)
+
+app.use(express.static('public'))
+const game = createGame()
+game.start()
+
+game.subscribe((command) => {
+    console.log(`Emitting ${command.type}`)
+    sockets.emit(command.type, command)
+})
+
+//game.addPlayer({playerId: 'player1', playerX: 1, playerY: 0})
+//game.addPlayer({playerId: 'player2', playerX: 1, playerY: 0})
+game.addFruit({fruitId: 'fruit1', fruitX: 4, fruitY: 7})
+game.addFruit({fruitId: 'fruit2', fruitX: 1, fruitY: 3})
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`> Player connected on server with id: ${playerId}`)
+
+    game.addPlayer({playerId: playerId})
+
+    socket.emit("setup", game.state)
+
+    socket.on('disconnect', () => {
+        game.removePlayer({ playerId: playerId})
+        console.log(`> Player disconnected: ${playerId}`)
+    })
+
+    socket.on('move-player', (command) => {
+        command.playerId = playerId
+        command.type = 'move-player'
+
+        game.movePlayer(command)
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`> Server listening on port: 3000`)
+})
